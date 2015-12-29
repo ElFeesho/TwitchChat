e29818752c864f69924ed61d7ecd13db402080f3
Chris Sawczuk <gummybassist@gmail.com>
2015-12-29 23:08:31 +0000

    removed comments

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     LoginDialog d;
     d.show();
-    //IrcChatWindow w("elfeesho", "#ismailzd");
-    //w.show();
 
     return a.exec();
 }
